19963d18b321c8c49c90e2afde838f8579ea89fc
Natali-QA-ooops <80644500+Natali-QA-ooops@users.noreply.github.com>
2021-04-03 13:16:19 +0300

    Added startGoogle

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+    WebDriver wd;
+
+    @Test
+    public void startgoogle(){
+        wd=new ChromeDriver();
+        wd.get("https://www.google.co.il/");
+    }
+}
